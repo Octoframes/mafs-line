7421c68b5e95e1c026fe4e4d6fe306683633b77a
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-24 17:21:19 +0200

    two component approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState, useRef } from "react";
 import { Mafs, Line, Coordinates, useMovablePoint } from "mafs";
 import "mafs/core.css";
 import "mafs/font.css";
@@ -7,22 +6,13 @@ function MyButton({ onClick }) {
   return <button onClick={onClick}> Hi </button>;
 }
 
-function MyComponent({ buttonval }) {
-  const point1 = useMovablePoint([-1, -1]);
-  const point2 = useMovablePoint([2, 1]);
-  const prevButtonVal = useRef(buttonval);
-
-  useEffect(() => {
-    if (buttonval !== prevButtonVal.current) {
-      point1.setPoint([point1.x + 1, point1.y]);
-    }
-    prevButtonVal.current = buttonval;
-  }, [buttonval]);
+function MyComponent({ point1, point2, setPoint1 }) {
 
   return (
     <Mafs viewBox={{ x: [-2, 2], y: [-1, 1] }}>
       <Coordinates.Cartesian />
       <Line.Segment point1={point1.point} point2={point2.point} />
+      {setPoint1.element}
       {point1.element}
       {point2.element}
     </Mafs>
@@ -30,16 +20,17 @@ function MyComponent({ buttonval }) {
 }
 
 export default function App() {
-  const [buttonval, setButtonVal] = useState(0);
+  const point1 = useMovablePoint([-1, -1]);
+  const point2 = useMovablePoint([2, 1]);
 
-  const handleClick = () => {
-    setButtonVal(buttonval + 1);
+  const handleButtonClick = () => {
+    point1.setPoint([point1.x + 1, point1.y]);
   };
 
   return (
     <>
-      <MyComponent buttonval={buttonval} />
-      <MyButton onClick={handleClick} />
+      <MyComponent point1={point1} point2={point2} setPoint1={point1.setPoint}/>
+      <MyButton onClick={handleButtonClick} />
     </>
   );
 }
