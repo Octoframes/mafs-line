7377f94fada9a650be50b8c4fa4e9499165f96f1
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-24 14:42:16 +0200

    now I'm in a loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "mafs/core.css";
 import "mafs/font.css";
 import { Mafs, Line, Coordinates, useMovablePoint } from "mafs";
 
-function Button({onClick}) {
+function Button({ onClick }) {
   return <button onClick={onClick}> Hi </button>;
-};
+}
 
 export default function App() {
-  const [color, setColor] = useState('red');
-  const [my_x, setmy_x] = useState(-2)
-  const point1 = useMovablePoint([my_x, -1]);
+  const [color, setColor] = useState("red");
+  const [my_x, setmy_x] = useState(-2);
+  const point1 = useMovablePoint([my_x, -1], {
+    onMove: ([x, _]) => setmy_x(x),
+  });
+
   const point2 = useMovablePoint([2, 1]);
 
   useEffect(() => {
     point1.setPoint([my_x, -1]);
   }, [my_x]);
 
-  
+
+  useEffect(() => {
+    console.log("hi");
+    setmy_x(point1.point[0]);
+  }, [point1.point]);
+
+
   function callback(color) {
     return color === "red" ? "blue" : "red";
   }
@@ -29,20 +38,20 @@ export default function App() {
   }
 
   function handleClick() {
-    setColor(prevColor => callback(prevColor));
-    setmy_x(prevVal => callback2(prevVal));
+    setColor((prevColor) => callback(prevColor));
+    setmy_x((prevVal) => callback2(prevVal));
   }
 
   return (
     <>
-    <Mafs viewBox={{ x: [-2, 2], y: [-1, 1] }}>
-      <Coordinates.Cartesian />
-      <Line.Segment point1={point1.point} point2={point2.point} />
-      {point1.element}
-      {point2.element}
-    </Mafs>
-    <Button onClick={handleClick}/>
-    <h1 style={{color: color}}> Hello World </h1>
+      <Mafs viewBox={{ x: [-2, 2], y: [-1, 1] }}>
+        <Coordinates.Cartesian />
+        <Line.Segment point1={point1.point} point2={point2.point} />
+        {point1.element}
+        {point2.element}
+      </Mafs>
+      <Button onClick={handleClick} />
+      <h1 style={{ color: color }}> Hello World </h1>
     </>
   );
 }
