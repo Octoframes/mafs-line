a1c41b0fb9c1c57a92eacf53693c2cdc4714d1fb
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-24 16:11:16 +0200

    minimal version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import "mafs/core.css";
 import "mafs/font.css";
 import { Mafs, Line, Coordinates, useMovablePoint } from "mafs";
@@ -8,24 +7,12 @@ function Button(probs) {
 }
 
 export default function App() {
-  const [color, setColor] = useState("red");
 
   const point1 = useMovablePoint([-1, -1]);
   const point2 = useMovablePoint([2, 1]);
 
-  useEffect(() => {
-    console.log(point1.x)
-  }, [point1.x])
-
-
-  function callback(color) {
-    return color === "red" ? "blue" : "red";
-  }
-
   function handleClick() {
-    console.log(point1.x)
     point1.setPoint([point1.x+1, point1.y]);
-    setColor((prevColor) => callback(prevColor));
   }
 
   return (
@@ -37,7 +24,6 @@ export default function App() {
         {point2.element}
       </Mafs>
       <Button onClick={handleClick} />
-      <h1 style={{ color: color }}> Hello World </h1>
     </>
   );
 }
