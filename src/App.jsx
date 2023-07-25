e3f32e4b18ed107faacc2b262240dcd13361dbab
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-25 17:29:32 +0200

    polar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,31 @@
 import { Mafs, Line, Coordinates, useMovablePoint } from "mafs";
-import "mafs/core.css";
-import "mafs/font.css";
 
-function MyButton({ onClick }) {
-  return <button onClick={onClick}> Hi </button>;
-}
+import "mafs/core.css";
+// import "mafs/font.css";
 
 export default function App() {
-  const point1 = useMovablePoint([-1, -1]);
-  const point2 = useMovablePoint([2, 1]);
-
-  const handleButtonClick = () => {
-    point1.setPoint([point1.x + 1, point1.y]);
-  };
+  const viewBox = { x: [-2, 2], y: [-4, 4] };
+  const point1 = useMovablePoint([-2, -1]);
 
   return (
-    <>
-      <Mafs viewBox={{ x: [-2, 2], y: [-1, 1] }}>
-        <Coordinates.Cartesian />
-        <Line.Segment point1={point1.point} point2={point2.point} />
-        {point1.element}
-        {point2.element}
-      </Mafs>
-      <MyButton onClick={handleButtonClick} />
-    </>
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div style={{ flex: 1, marginRight: "10px" }}>
+        <Mafs viewBox={viewBox} pan={false}>
+          <Coordinates.Cartesian />
+          <Line.Segment point1={[0,0]} point2={[point1.point[0],0]} color="yellow" />
+          <Line.Segment point1={[point1.point[0],0]} point2={point1.point} color="yellow" />
+
+
+          {point1.element}
+        </Mafs>
+      </div>
+
+      <div style={{ flex: 1, marginLeft: "10px" }}>
+        <Mafs viewBox={viewBox} pan={false}>
+          <Coordinates.Polar subdivisions={3} lines={1} />
+          {point1.element}
+        </Mafs>
+      </div>
+    </div>
   );
 }
