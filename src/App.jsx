6e920303d069b1ad3bdfed7e5c8191a8adadc151
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-25 18:15:19 +0200

    more polar tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import * as React from "react";
 import { Mafs, Line, Coordinates, useMovablePoint, Plot } from "mafs";
-import "mafs/core.css";
+import "./styles.css";
 
-export default function App() {
+export default function PolarCoordinate() {
   const viewBox = { x: [-2, 2], y: [-4, 4] };
-  const point1 = useMovablePoint([-2, -1]);
+  const point1 = useMovablePoint([-2, -1],  {
+    color: "orange"});
   const r = Math.sqrt(point1.point[0] ** 2 + point1.point[1] ** 2);
   let angle = Math.atan2(point1.point[1], point1.point[0]);
-  
+  const mycolor = "blue";
   // Adjust angle to always be positive, between 0 to 2π
   if (angle < 0) {
     angle += 2 * Math.PI;
@@ -14,36 +16,32 @@ export default function App() {
 
   return (
     <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <div style={{ flex: 1, marginRight: "10px" }}>
+      <div style={{ flex: 1, marginRight: "10px", background: "blue" }}>
         <Mafs viewBox={viewBox} pan={false}>
           <Coordinates.Cartesian />
           <Line.Segment
             point1={[0, 0]}
             point2={[point1.point[0], 0]}
-            color="yellow"
+            color={mycolor}
           />
           <Line.Segment
             point1={[point1.point[0], 0]}
             point2={point1.point}
-            color="yellow"
+            color={mycolor}
           />
 
           {point1.element}
         </Mafs>
       </div>
 
-      <div style={{ flex: 1, marginLeft: "10px" }}>
+      <div style={{ flex: 1, marginLeft: "10px", background: "blue" }}>
         <Mafs viewBox={viewBox} pan={false}>
-          <Coordinates.Polar subdivisions={3} lines={1} />
-          <Line.Segment
-            point1={[0, 0]}
-            point2={[r,0]}
-            color="yellow"
-          />
+          <Coordinates.Polar subdivisions={1} lines={1} />
+          <Line.Segment point1={[0, 0]} point2={[r, 0]} color={mycolor} />
           <Plot.Parametric
             t={[0, angle]}
             xy={(t) => [r * Math.cos(t), r * Math.sin(t)]}
-            color="yellow"
+            color={mycolor}
           />
           {point1.element}
         </Mafs>
